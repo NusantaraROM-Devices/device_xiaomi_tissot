d9df0ed8439823a35a4bc979bd778d35c1dbc718
yincen17 <yincen17@users.noreply.github.com>
2020-08-09 00:33:42 +0000

    commit: tissot bringup nusantara,

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tissot.mk
+    $(LOCAL_DIR)/nad_tissot.mk
+COMMON_LUNCH_CHOICES := \
+    nad_whyred-user \ 
+    nad_whyred-userdebug \
+    nad_whyred-eng
