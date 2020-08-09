2a35b4573841b62a0e3ef499d496ce7496377859
yincen17 <yincen17@users.noreply.github.com>
2020-08-09 00:55:53 +0000

    forget to change whyred to tissot

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,6 +16,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/nad_tissot.mk
 COMMON_LUNCH_CHOICES := \
-    nad_whyred-user \ 
-    nad_whyred-userdebug \
-    nad_whyred-eng
+    nad_tissot-user \
+    nad_tissot-userdebug \
+    nad_tissot-eng
