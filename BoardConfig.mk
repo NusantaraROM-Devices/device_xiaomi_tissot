225ae4eee6c45cda93088ec6ee19fab84f6d576a
yincen17 <52388255+yincen17@users.noreply.github.com>
2021-01-17 21:27:01 +0800

    Set Selinux Neverallow

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,3 +51,6 @@ BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/tissot/BoardConfigVendor.mk
+
+# Selinux
+SELINUX_IGNORE_NEVERALLOWS := true
