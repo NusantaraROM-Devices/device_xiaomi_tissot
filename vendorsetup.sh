774fbc51bba47a84a81870b87f822f3aa2855e97
yincen17 <52388255+yincen17@users.noreply.github.com>
2021-03-24 22:24:32 +0800

    clone hals

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,11 @@
 rm -rf hardware/qcom-caf/wlan 
 git clone https://github.com/ArrowOS/android_hardware_qcom_wlan.git -b arrow-11.0-caf hardware/qcom-caf/wlan
 echo "Cloned arrow wlan"
+rm -rf hardware/qcom-caf/msm8996/audio
+rm -rf hardware/qcom-caf/msm8996/display  
+rm -rf hardware/qcom-caf/msm8996/media
+echo "delete hals"
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-18.0-caf-msm8996 hardware/qcom-caf/msm8996/display
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.0-caf-msm8996 hardware/qcom-caf/msm8996/media
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.0-caf-msm8996 hardware/qcom-caf/msm8996/audio
+echo "cloned hals"
