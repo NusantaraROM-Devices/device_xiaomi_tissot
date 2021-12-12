983dee7e8035c54f9754fc3a78d7f37c5d499d96
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2021-12-12 12:14:45 +0700

    tissot: Nusantara Project
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2017-2021 The LineageOS Project
+# Copyright (C) 2017-2021 The Nusantara Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tissot.mk
+    $(LOCAL_DIR)/nad_tissot.mk
+
+COMMON_LUNCH_CHOICES := \
+    nad_tissot-user \
+    nad_tissot-userdebug \
+    nad_tissot-eng
